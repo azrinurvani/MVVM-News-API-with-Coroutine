db813b99686d80d2ac8a067623b67dcdfa10fb69
azrinurvani <azrinurvani@gmail.com>
2022-02-23 16:28:59 +0700

    Step 3 - DAO and Article Setup ROOM

---
+++ app/src/main/java/com/mobile/azri/mvvmnewsapiwithcoroutine/db/ArticleDao.kt
@@ -0,0 +1,24 @@
+package com.mobile.azri.mvvmnewsapiwithcoroutine.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.mobile.azri.mvvmnewsapiwithcoroutine.model.Article
+
+@Dao
+interface ArticleDao {
+
+    /*onConflict berfungsi untuk memberi action di database apabila insert data baru,
+     jika terdapat data lama maka akan di replace
+    * */
+
+    /*pada coroutine wajib memakai method suspend*/
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticle(article: Article) : Long
+
+    /*Method dengan return Live Data tidak bisa bekerja di method suspend*/
+    @Query("SELECT * FROM articles")
+    fun getAllArticles() : LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
